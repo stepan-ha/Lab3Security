6364254f7ddc0b72d963bac7031079f6a8a8e53c
stepanhalaiko <stepanhalaiko@gmail.com>
2023-11-02 19:17:31 +0200

    changed vector

--- Randomizer.py
+++ Randomizer.py
@@ -4,11 +4,10 @@ class Randomizer:
 
     def Generate(self, n):
         resArr = []
-        step = 64
+        step = random.randint(1, pow (2,15))
 
         resArr.append(step)
-        randomM = random.randint(1, pow (2,15));
         for i in range(1, n):
-            resArr.append(((8 * resArr[i - 1]) + 8) % randomM-1)
+            resArr.append(((8 * resArr[i - 1]) + 8) % pow (2,15)-1)
 
         return  resArr
